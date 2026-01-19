6b0c69d17e1c45658646b74a3b41407910ebe403
Deekshitha <vegirajudeekshitha@gmail.com>
2026-01-19 11:02:32 +0530

    mes1

--- IV-SEM/python/Practice/M01_Language_and_Programming_Basis/S02/PS03_operators.py
+++ IV-SEM/python/Practice/M01_Language_and_Programming_Basis/S02/PS03_operators.py
@@ -5,3 +5,7 @@ print(~x)
 
 s = input()
 print("on" in s)
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3]
+print(l1 is l2)
---
+++ IV-SEM/python/Practice/M01_Language_and_Programming_Basis/S02/PS04_conditional_statements.py
@@ -0,0 +1,16 @@
+#even or odd
+num = int(input())
+if(num % 2 == 0):
+    print("Even")
+else:
+    print("False")
+
+
+#positive, negative or zero
+num = int(input())
+if(num > 0):
+    print("Positive")
+elif(num == 0):
+    print("zero")
+else:
+    print("Negative")
